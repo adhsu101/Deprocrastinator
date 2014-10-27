fe3afe69c8b89c5506c1803e27f95a4bcf856309
Mobile Making <mm@Alexs-MacBook-Pro-2.local>
2014-10-27 15:47:40 -0700

    implemented checkmarks

--- Deprocrastinator/RootViewController.m
+++ Deprocrastinator/RootViewController.m
@@ -12,6 +12,7 @@
 
 @property (strong, nonatomic) IBOutlet UITextField *toDoTextField;
 @property NSMutableArray *toDoList;
+@property NSMutableArray *checkmarks;
 @property (strong, nonatomic) IBOutlet UITableView *toDoListTableView;
 
 @end
@@ -23,6 +24,7 @@
     [super viewDidLoad];
 
     self.toDoList = [@[] mutableCopy];
+    self.checkmarks = [@[] mutableCopy];
 }
 
 #pragma mark - tableview delegates
@@ -37,6 +39,13 @@
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"toDoItemCell" forIndexPath:indexPath];
 
     cell.textLabel.text = self.toDoList[indexPath.row];
+    if ([self.checkmarks[indexPath.row] isEqual:@YES]) {
+        cell.accessoryType = UITableViewCellAccessoryCheckmark;
+    }
+    else
+    {
+        cell.accessoryType = UITableViewCellAccessoryNone;
+    }
 
     return cell;
 }
@@ -45,9 +54,28 @@
 - (IBAction)onAddButtonPressed:(id)sender
 {
     [self.toDoList addObject:self.toDoTextField.text];
+    [self.checkmarks addObject:@NO];
     [self.toDoListTableView reloadData];
     [self.toDoTextField resignFirstResponder];
     self.toDoTextField.text = @"";
+    
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    NSNumber *isChecked = [self.checkmarks objectAtIndex:indexPath.row];
+    
+    if ([isChecked isEqual:@NO])
+    {
+        [self.checkmarks replaceObjectAtIndex:indexPath.row withObject:@YES];
+    }
+    else
+    {
+        [self.checkmarks replaceObjectAtIndex:indexPath.row withObject:@NO];
+    }
+
+    [self.toDoListTableView reloadData];
+
 }
 
 
