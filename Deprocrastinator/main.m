b4392fbec84bb9c4f2e056d6b53ce420c096eb1d
Mobile Making <mm@Alexs-MacBook-Pro-2.local>
2014-10-27 14:04:14 -0700

    Initial Commit

---
+++ Deprocrastinator/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Deprocrastinator
+//
+//  Created by Mobile Making on 10/27/14.
+//  Copyright (c) 2014 Alex Hsu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
